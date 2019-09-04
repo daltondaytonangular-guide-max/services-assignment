016e67575513f4bc3546f2f08f210c7465ccbd68
Dalton Dayton <daltondayton1@gmail.com>
2019-09-03 21:23:07 -0600

    UsersService Solution

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -4,5 +4,16 @@ import { Injectable } from '@angular/core';
     providedIn: 'root',
 })
 export class UsersService {
-    constructor() {}
+    activeUsers = ['Max', 'Anna'];
+    inactiveUsers = ['Chris', 'Manu'];
+
+    setToActive(id: number) {
+        this.activeUsers.push(this.inactiveUsers[id]);
+        this.inactiveUsers.splice(id, 1);
+    }
+
+    setToInactive(id: number) {
+        this.inactiveUsers.push(this.activeUsers[id]);
+        this.activeUsers.splice(id, 1);
+    }
 }
